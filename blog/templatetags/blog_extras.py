92ea4db8d5f0cd45d350024c83109e7f24a78655
Rafael Andres <r.andres.blasco@gmail.com>
2022-10-09 18:49:29 +0000

    Finish crispy forms

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -31,8 +31,8 @@ def author_details(author, current_user=None):
   return format_html('{}{}{}', prefix, name, suffix)
 
 @register.simple_tag
-def row():
-  return format_html('<div class="row">')
+def row(extra_classes=""):
+  return format_html('<div class="row {}">', extra_classes)
 
 @register.simple_tag
 def endrow():
